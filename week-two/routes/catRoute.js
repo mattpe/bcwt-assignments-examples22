ea2de5de0d9002d34ca3a3b466fdf027d448d74f
mattpe <mattpe@metropolia.fi>
2022-11-01 15:17:08 +0200

    add router for /cat

--- week-two/routes/catRoute.js
+++ week-two/routes/catRoute.js
@@ -1,2 +1,24 @@
 'use strict';
-// catRoute
+// catRoutes
+const express = require('express');
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  //console.log(req);
+  res.send('From this endpoint you can get cats.')
+});
+router.get('/:catId', (req, res) => {
+  //console.log(req.params);
+  res.send('From this endpoint you can get cat with id ' + req.params.catId);
+});
+router.post('/', (req, res) => {
+  res.send('From this endpoint you can add more cats.');
+});
+router.put('/', (req, res) => {
+  res.send('From this endpoint you can edit cats.');
+});
+router.delete('/', (req, res) => {
+  res.send('From this endpoint you can delete cats.');
+});
+
+module.exports = router;
