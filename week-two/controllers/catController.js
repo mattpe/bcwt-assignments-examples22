777d1b95b1328dc05bcfcef47b2aa52e998e5fbb
mattpe <mattpe@metropolia.fi>
2022-11-02 15:58:46 +0200

    get a cat by id added

--- week-two/controllers/catController.js
+++ week-two/controllers/catController.js
@@ -2,19 +2,20 @@
 const catModel = require('../models/catModel');
 
 const getCats = async (req, res) => {
-  const cats = await catModel.getAllCats();
+  const cats = await catModel.getAllCats(res);
   res.json(cats);
 };
 
-const getCat = (req, res) => {
+const getCat = async (req, res) => {
   // choose only one object with matching id
-  const cat = cats.filter(cat => req.params.catId == cat.id)[0];
+  const cat = await catModel.getCatById(res, req.params.catId);
   if (cat) {
     res.json(cat);
   } else {
     res.sendStatus(404);
   }
 };
+
 const createCat = (req, res) => {
   console.log(req.body);
   res.send('adding a cat');
