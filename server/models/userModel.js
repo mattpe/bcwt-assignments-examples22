65a7e4069988706659ede93e2a9ccae15aabae36
mattpe <mattpe@metropolia.fi>
2022-11-23 10:18:03 +0200

    wip: add passport auth

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -23,6 +23,18 @@ const getUserById = async (id, res) => {
     res.status(500).send(e.message);
   }
 };
+const getUserLogin = async (user) => {
+  try {
+    console.log('getUserLogin()', user);
+    const [rows] = await promisePool.execute(
+        'SELECT * FROM wop_user WHERE email = ?;',
+        user);
+    return rows;
+  } catch (e) {
+    console.error("error", e.message);
+    res.status(500).send(e.message);
+  }
+};
 const addUser = async (user, res) => {
   try {
     const sql = 'INSERT INTO wop_user VALUES (null, ?, ?, ?, ?)';
@@ -38,5 +50,6 @@ const addUser = async (user, res) => {
 module.exports = {
   getAllUsers,
   getUserById,
-  addUser
+  getUserLogin,
+  addUser,
 };
